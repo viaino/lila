a507729c8e5f015236838113abc157e9b4056ceb
inocelen <ino@celen.jp>
2019-12-06 19:44:46 +0900

    no message

--- assets/main.js
+++ assets/main.js
@@ -8,3 +8,20 @@ function closeNav() {
 	var target = document.getElementById('navbarNav');
 	target.classList.toggle('show');
 }
+
+// スムーススクロール
+$(function(){
+  $('a[href^="#mv"]').click(function(){
+    //スクロールのスピード
+    var speed = 1000;
+    //リンク元を取得
+    var href= $(this).attr("href");
+    //リンク先を取得
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    //リンク先までの距離を取得
+    var position = target.offset().top-50;
+    //スムーススクロール
+    $("html, body").animate({scrollTop:position}, speed, "swing");
+    return false;
+});
+});
